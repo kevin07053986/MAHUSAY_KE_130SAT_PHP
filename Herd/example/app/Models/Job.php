1416d48f707284a3eb70ae074f593df3221ae345
Kevin Mahusay <kevinmahusay22@gmail.com>
2024-10-10 04:31:39 +0800

    10-10_video-9-eloquent

--- Herd/example/app/Models/Job.php
+++ Herd/example/app/Models/Job.php
@@ -2,42 +2,50 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => 'Php 50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => 'Php 40,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => 'Php 30,000'
-            ]
-            ];
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        // Arr::first($jobs, function ($job) use ($id){
-        //     return $job['id'] == $id;
-        // });  
+    protected $fillable = ['title', 'salary'];
 
-        if (! $job){
-            abort (404);
-        }   
-        
-        return $job;
-    }    
 }
+
+
+
+// public static function all(): array
+// {
+//     return [
+//         [
+//             'id' => 1,
+//             'title' => 'Director',
+//             'salary' => 'Php 50,000'
+//         ],
+//         [
+//             'id' => 2,
+//             'title' => 'Programmer',
+//             'salary' => 'Php 40,000'
+//         ],
+//         [
+//             'id' => 3,
+//             'title' => 'Teacher',
+//             'salary' => 'Php 30,000'
+//         ]
+//         ];
+// }
+
+// public static function find(int $id): array
+// {
+//     $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+//     // Arr::first($jobs, function ($job) use ($id){
+//     //     return $job['id'] == $id;
+//     // });  
+
+//     if (! $job){
+//         abort (404);
+//     }   
+    
+//     return $job;
+// }    
 
