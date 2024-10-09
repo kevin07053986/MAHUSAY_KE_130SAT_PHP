532e96a36d51d13ff190ec0f55c5b65ca641ab73
Kevin Mahusay <kevinmahusay22@gmail.com>
2024-10-09 15:02:19 +0800

    10/09_video-3_create-the-obligatory-three-page-layout-using-layout-files

--- Herd/example/resources/views/Components/layout.blade.php
+++ Herd/example/resources/views/Components/layout.blade.php
@@ -5,10 +5,17 @@
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie-edge">
 
-        <title>Contact Page</title>
+        <title>Home Page</title>
         
     </head>
     <body>
-        <h1> Hello from the Contact Page.</h1>
+        <nav>
+            <a href="/"> Home </a>
+            <a href="/about"> About </a>
+            <a href="/contact"> Contact </a>
+        </nav>
+
+        {{ $slot }} <!-- is the same with <?php echo $slot ?> -->
+        
     </body>
 </html>
